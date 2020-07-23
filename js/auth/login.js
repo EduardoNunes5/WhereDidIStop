264c7436ffd6384f69773f4ba893608665ad1851
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-07-23 03:01:49 -0300

    Url path

--- js/auth/login.js
+++ js/auth/login.js
@@ -6,19 +6,19 @@ const $signUp = document.querySelector('#signup');
 console.log($signUp);
 $signUp.addEventListener('click', (e)=> {
     e.preventDefault();
-    window.location.href = '/signup.html';
+    window.location.href = './signup.html';
 });
 
 $form.addEventListener('submit', async(e)=>{
     e.preventDefault();
-    const email = document.querySelector('#email').value;
+    const email = document.querySelector('#email').value.toLowerCase();
     const password = document.querySelector('#password').value;
     const result = await post({email, password},null,'/login');
     const json = await result.json();
     const token = json.token;
     if(token){
         sessionStorage.setItem('token', token);
-        window.location.href="/index.html";
+        window.location.href="./index.html";
     }
     else{
         alert(json.msg);
