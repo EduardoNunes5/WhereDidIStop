ee7a0c017596c2b6a46151e5e3ac217b58789d67
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-07-23 02:43:09 -0300

    Path href

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const $add = document.querySelector('#add');
 const $logout = document.querySelector('#logout');
 const token = sessionStorage.getItem('token');
 if(!token){
-    window.location.href = '/login.html';
+    window.location.href = 'WhereDidIStop/login.html';
 }
 else{
     $add.hidden = false;
