cd2e6386e6944c7f2b14a91382966a275f0ecedd
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-07-23 03:02:35 -0300

    Url path

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -5,7 +5,7 @@ const $signin = document.querySelector('#login');
 
 $signin.addEventListener('click', (e)=>{
     e.preventDefault();
-    window.location.href = '/login.html';
+    window.location.href = './login.html';
 })
 
 $form.addEventListener('submit', async(e)=>{
@@ -15,7 +15,7 @@ $form.addEventListener('submit', async(e)=>{
     const password = document.querySelector('#password').value;
     const result = await post({name, email, password},null, 'signup');
     if(result.status === 201){
-        window.location.href="/login.html"
+        window.location.href="./login.html"
     }
     if(result.status === 409){
         alert('Email já cadastrado! Tente novamente com outro email.')
